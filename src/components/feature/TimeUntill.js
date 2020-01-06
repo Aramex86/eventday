dfe323daf060e8028a1636e0255d9c15ca31ad88
Slava <slavbez@mail.ru>
2020-01-06 22:29:27 +0200

    Countdown refresh

--- src/components/feature/TimeUntill.js
+++ src/components/feature/TimeUntill.js
@@ -4,7 +4,7 @@ import Slide from 'react-reveal/Slide';
 
 class TimeUntill extends Component{
     state ={
-        deadline:'Dec, 16 , 2019',
+        deadline:'Jul, 16 , 2020',
         days:'0',
         hours:'0',
         minutes:'0',
